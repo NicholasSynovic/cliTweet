ef5fc99eba639fc45d6f9382f24081387a6c8a3b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-07-03 17:52:58 -0500

    Properly write to file

--- hushtweet/utils/credentials.py
+++ hushtweet/utils/credentials.py
@@ -2,7 +2,10 @@ import toml
 
 
 def writeTOML(data: dict) -> str:
-    return toml.dump(data, ".htconfig.toml")
+    with open(".htconfig.toml", "w") as tomlFile:
+        data: str = toml.dump(data, tomlFile)
+        tomlFile.close()
+    return data
 
 
 def readTOML() -> dict:
